4d7aa8bb3d98a6e557c89468e9a92c37927d436f
longliuping <longliuping@live.cn>
2015-09-12 15:55:39 +0800

    testddd

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Test git hub  https://github.com/longliuping/testgithub.git");
+		System.out.println("Test git hub  https://github.com/longliuping/testgithub.git  fdsfdsf");
 	}
 }
