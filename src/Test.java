1f9cf667f95926e9a5d914d270a9685e798c2ee3
longliuping <longliuping@live.cn>
2015-09-12 17:11:13 +0800

    longliuping ceo

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test git hub  https://github.com/longliuping/testgithub.git  fdsfdsf,,,,longliuping7");
 		System.out.println("longliuping new");
+		System.out.println("longliuping ceo");
 	}
 }
