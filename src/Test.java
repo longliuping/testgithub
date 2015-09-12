789e1dd70889e815f79673fb09cb07d251b50ba1
longliuping <longliuping@live.cn>
2015-09-12 16:52:49 +0800

    new

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test git hub  https://github.com/longliuping/testgithub.git  fdsfdsf,,,,longliuping7");
-		System.out.println("longliuping");
+		System.out.println("longliuping new");
 	}
 }
