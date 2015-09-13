07ad159333c2d2718bb404ada630a391a21994a3
longliuping7 <1115955757@qq.com>
2015-09-13 13:14:20 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test git hub  https://github.com/longliuping/testgithub.git  fdsfdsf,,,,longliuping7");
 		System.out.println("longliuping new");
-		System.out.println("longliuping ceo  7777");
+		System.out.println("longliuping ceo  7777   11111");
 		
 	}
 }
